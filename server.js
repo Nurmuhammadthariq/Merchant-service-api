a8b318ff85a3d8ab1c6f780701fa62f535aee7c6
thoriqnoer <thoriq.noer@gmail.com>
2022-02-17 12:59:44 +0700

    add route and middleware

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
 const port = 5000;
 const db = require('./config/Database');
+const dotenv = require('dotenv');
+
+// Routes
+const userRoutes = require('./app/routes/userRoutes');
+
+dotenv.config();
+const app = express();
 
 try {
   async function connect() {
@@ -13,9 +19,11 @@ try {
   console.log(error);
 }
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
