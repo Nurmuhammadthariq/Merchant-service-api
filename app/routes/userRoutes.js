9c16a18a1ab3e62b6dc79817198bf7b9b94ba5da
thoriqnoer <thoriq.noer@gmail.com>
2022-02-23 18:58:09 +0700

    add auth contoller

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers, registerUser } = require('../controllers/Users');
+const {
+  getUsers,
+  registerUser,
+  authUser,
+  getUserProfile,
+} = require('../controllers/Users');
+const { protect } = require('../middlewares/authMiddleware.js');
 
-router.get('/', getUsers);
+router.get('/', protect, getUsers);
 router.post('/register', registerUser);
+router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
+
 module.exports = router;
